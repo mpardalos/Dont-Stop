e6d6eada5d366b72034a7d72e95a2a2773185a02
Michalis Pardalos <mpardalos@gmail.com>
2017-05-27 14:03:34 +0300

    Add basic player sprite and input

---
+++ Assets/Scripts/InputManager.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputManager : MonoBehaviour {
+    private Player m_Player;
+
+	void Start () {
+        m_Player = GameObject.Find("Player").GetComponent<Player>();		
+	}
+	
+	void Update() {
+        m_Player.Move(new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")));
+	}
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+    public float MoveSpeed;
+
+    private Rigidbody2D m_Rigidbody;
+
+    void Start() {
+        m_Rigidbody = GetComponent<Rigidbody2D>();
+    }
+    
+    public void Move(Vector2 direction) {
+        Debug.Log(direction.normalized * MoveSpeed);
+        m_Rigidbody.velocity = direction.normalized * MoveSpeed;
+    }
+}
