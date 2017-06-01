7a6b99d1482812eb2401dcd7ab2f75241993aba2
Michalis Pardalos <mpardalos@gmail.com>
2017-06-01 20:01:07 +0300

    Show health as a bar

--- Assets/Scripts/HealthDisplay.cs
+++ Assets/Scripts/HealthDisplay.cs
@@ -5,26 +5,19 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class HealthDisplay : MonoBehaviour {
-    public int MaxHealth;
-    
+    public int MaxHealth; 
+    public RectTransform FillTransform;
+
     public int Health {
-        get {
-            return int.Parse(m_Display.text);
-        } 
         set {
-            if (0 <= value && value <= MaxHealth) {
-                m_Display.text = value.ToString();
-            } else {
-                throw new ArgumentException("Health must be between 0 and MaxHealth");
-            }
+            FillTransform.localScale = new Vector3((float)value/(float)MaxHealth, 1, 1);
+            Debug.Log(FillTransform.localScale);
         }
     }
 
-    private Text m_Display;
 
     void Start() {
-        m_Display = gameObject.transform.Find("Display").gameObject.GetComponent<Text>();
-        Health    = MaxHealth;
+        Health = MaxHealth;
     }
             
 }
